264b8e70f6f22a09857564cdbac5a25488e17b53
Boris <yorkego1@gmail.com>
2020-06-05 21:57:32 +0300

    create pagination in Users

--- src/Redux/users-reducer.js
+++ src/Redux/users-reducer.js
@@ -1,9 +1,14 @@
 const FOLLOW = 'FOLLOW'
 const UNFOLLOW = 'UNFOLLOW'
 const SET_USERS = 'SET-USERS'
+const SET_CURRENT_PAGE = 'SET-CURRENT-PAGE'
+const SET_TOTAL_USER_COUNT = 'SET-TOTAL-USER-COUNT'
 
 let initialState = {
-    users: []
+    users: [],
+    pageSize: 5,
+    totalUsersCount: 0,
+    currentPage: 1
 }
 const usersReducer = (state = initialState, action) => {
     switch (action.type) {
@@ -28,7 +33,11 @@ const usersReducer = (state = initialState, action) => {
                 })
             }
         case SET_USERS:
-            return { ...state, users: [...state.users, ...action.users] }
+            return { ...state, users: action.users }
+        case SET_CURRENT_PAGE:
+            return { ...state, currentPage: action.currentPage }
+        case SET_TOTAL_USER_COUNT:
+            return { ...state, totalCount: action.count }
         default:
             return state
     }
@@ -37,4 +46,6 @@ const usersReducer = (state = initialState, action) => {
 export const followActionCreator = (userId) => ({ type: FOLLOW, userId })
 export const unfollowActionCreator = (userId) => ({ type: UNFOLLOW, userId })
 export const setUsersActionCreator = (users) => ({ type: SET_USERS, users })
+export const setCurrentPageActionCreator = (currentPage) => ({ type: SET_CURRENT_PAGE, currentPage })
+export const setTotalUsersCountActionCreator = (totalUsersCount) => ({ type: SET_TOTAL_USER_COUNT, count: totalUsersCount})
 export default usersReducer
--- src/components/Users/UsersContainer.jsx
+++ src/components/Users/UsersContainer.jsx
@@ -1,11 +1,14 @@
 // import React from 'react'
 import { connect } from 'react-redux'
 import Users from './Users'
-import { followActionCreator, unfollowActionCreator, setUsersActionCreator } from '../../Redux/users-reducer'
+import { followActionCreator, unfollowActionCreator, setUsersActionCreator, setCurrentPageActionCreator, setTotalUsersCountActionCreator } from '../../Redux/users-reducer'
 
 let mapStateToProps = (state) => {
     return {
-        users: state.usersPage.users
+        users: state.usersPage.users,
+        pageSize: state.usersPage.pageSize,
+        totalUsersCount: state.usersPage.totalUsersCount,
+        currentPage: state.usersPage.currentPage
     }
 }
 let mapDispatchToProps = (dispatch) => {
@@ -18,6 +21,12 @@ let mapDispatchToProps = (dispatch) => {
         },
         setUsers: (users) => {
             dispatch(setUsersActionCreator(users))
+        },
+        setCurrentPage: (pageNumber) => {
+            dispatch(setCurrentPageActionCreator(pageNumber))
+        },
+        setTotalUsersCount: (totalCount) => {
+            dispatch(setTotalUsersCountActionCreator(totalCount))
         }
     }
 }
