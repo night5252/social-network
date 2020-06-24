b94d52499d41404ae19e574dca3ad88a44279c49
Boris <yorkego1@gmail.com>
2020-06-24 11:23:48 +0300

    create api.js with axios

---
+++ src/api/api.js
@@ -0,0 +1,18 @@
+import * as axios from 'axios'
+
+const instance = axios.create({
+    withCredentials: true,
+    baseURL: 'https://social-network.samuraijs.com/api/1.0/',
+    headers: {
+        "API-KEY": "38bf6a92-ae0a-4062-a0bf-9da7c1e77872"
+    }
+})
+
+export const userAPI = {
+    getUsers(currentPage = 1, pageSize = 5) {
+        return instance.get(`users?page=${currentPage}&count=${pageSize}`)
+            .then(response => {
+                return response.data
+            })
+    }
+}
