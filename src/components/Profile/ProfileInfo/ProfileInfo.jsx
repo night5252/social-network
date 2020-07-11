15aec73bf5dc63f75bbfe3cbc8938748787ecd68
Boris <yorkego1@gmail.com>
2020-07-11 20:57:12 +0300

    create ProfileStatus

--- src/components/Profile/ProfileInfo/ProfileInfo.jsx
+++ src/components/Profile/ProfileInfo/ProfileInfo.jsx
@@ -1,22 +1,23 @@
 import React from 'react'
 import styles from './ProfileInfo.module.css'
 import Preloader from '../../common/Preloader/Preloader'
+import ProfileStatus from './ProfileStatus'
 
 const ProfileInfo = (props) => {
   if (!props.profile) {
     return <Preloader />
   }
-    return (
-      <content>
-        <div>
-          <img alt='background' src='https://s.yimg.com/uu/api/res/1.2/DdytqdFTgtQuxVrHLDdmjQ--~B/aD03MTY7dz0xMDgwO3NtPTE7YXBwaWQ9eXRhY2h5b24-/https://media-mbst-pub-ue1.s3.amazonaws.com/creatr-uploaded-images/2019-11/7b5b5330-112b-11ea-a77f-7c019be7ecae' />
-        </div>
-        <div className={styles.descriptionBlock}>
-          <img alt='ava' src={props.profile.photos.large} />
-      ava+description
-    </div>
-      </content>
-    )
+  return (
+    <content>
+      <div>
+        {/* <img alt='background' src='https://s.yimg.com/uu/api/res/1.2/DdytqdFTgtQuxVrHLDdmjQ--~B/aD03MTY7dz0xMDgwO3NtPTE7YXBwaWQ9eXRhY2h5b24-/https://media-mbst-pub-ue1.s3.amazonaws.com/creatr-uploaded-images/2019-11/7b5b5330-112b-11ea-a77f-7c019be7ecae' /> */}
+      </div>
+      <div className={styles.descriptionBlock}>
+        <img alt='ava' src={props.profile.photos.large} />
+        <ProfileStatus status={'Hello my friends'} />
+      </div>
+    </content>
+  )
 }
 
 export default ProfileInfo
